139bddc92c843a4d6f27cdb439b9457f8e78671e
Ebrahim <81568567+eb-73@users.noreply.github.com>
2024-12-21 16:47:20 +0330

    fix: tailwind config issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
-  content: ["./index.html"],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+	darkMode: "class",
+	content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };
